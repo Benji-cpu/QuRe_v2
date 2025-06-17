a58a5f08778105329a47f9bb20da503bea2f205d
Ben Hemson-Struthers <profbenjo@gmail.com>
2025-06-17 08:11:29 +0800

    Fix configuration with proper minimal app.json and full app.config.js

--- config/app.config.ts
+++ config/app.config.ts
@@ -1,63 +1,64 @@
 // app.config.js
 export default {
-    expo: {
-      name: "QuRe",
-      slug: "QuRe",
-      version: "1.0.5",
-      orientation: "portrait",
-      icon: "./assets/images/icon.png",
-      scheme: "qure",
-      userInterfaceStyle: "automatic",
-      newArchEnabled: true,
-      ios: {
-        supportsTablet: true,
-        bundleIdentifier: "com.qure.app",
-        buildNumber: "6"
+  expo: {
+    name: "QuRe",
+    slug: "QuRe",
+    owner: "benji000", 
+    version: "1.0.4",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "qure",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    runtimeVersion: "1.0.4",
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.qure.app",
+      buildNumber: "6"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
       },
-      android: {
-        adaptiveIcon: {
-          foregroundImage: "./assets/images/adaptive-icon.png",
-          backgroundColor: "#ffffff"
-        },
-        edgeToEdgeEnabled: true,
-        package: "com.qure.app",
-        versionCode: 6
-      },
-      web: {
-        bundler: "metro",
-        output: "static",
-        favicon: "./assets/images/favicon.png"
-      },
-      plugins: [
-        "expo-router",
-        [
-          "expo-splash-screen",
-          {
-            "image": "./assets/images/splash-icon.png",
-            "imageWidth": 200,
-            "resizeMode": "contain",
-            "backgroundColor": "#ffffff"
-          }
-        ],
-        [
-          "react-native-iap",
-          {
-            "paymentProvider": "Play Store"
-          }
-        ]
-      ],
-      experiments: {
-        typedRoutes: true
-      },
-      extra: {
-        router: {},
-        eas: {
-          projectId: "382a05a5-832d-4999-980f-2d14a15d4111"
+      edgeToEdgeEnabled: true,
+      package: "com.qure.app",
+      versionCode: 6
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/splash-icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#2196f3"
         }
-      },
-      runtimeVersion: "1.0.2",
-      updates: {
-        url: "https://u.expo.dev/382a05a5-832d-4999-980f-2d14a15d4111"
+      ],
+      [
+        "react-native-iap",
+        {
+          "paymentProvider": "Play Store"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "3bb00633-0ef4-4ac5-918a-be69c0027772" 
       }
+    },
+    updates: {
+      url: "https://u.expo.dev/3bb00633-0ef4-4ac5-918a-be69c0027772"
     }
-  };
+  }
+};
